4805ad69f5288b03d0ea51d4ac38c1a2e03285ec
joaquin <joaquinre123@gmail.com>
2022-08-04 16:16:54 -0300

    Initial Commit

---
+++ frontend/src/components/shared/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className={styles.button}>
+        <span>{children}</span>
+        <img 
+            className={styles.arrow}
+            src="/images/arrow.png"
+            alt="arrow"
+        />
+    </button>
+  )
+}
+
+export default Button
