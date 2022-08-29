d310f10b408ebde4d0f0eca4394a5d3a5a5526b2
joaquin <joaquinre123@gmail.com>
2022-08-29 18:19:15 -0300

    onNext function

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -4,11 +4,16 @@ import Button from '../../../components/shared/Button/Button'
 import Card from '../../../components/shared/Card/Card'
 import TextInput from '../../../components/shared/TextInput/TextInput'
 import styles from  './StepOtp.module.css'
+import { verifyOtp } from '../../../http'
 
 const StepOtp = ({ onNext }) => {
   const [otp, setOtp] = useState('')
   async function submit() {
-    
+    try {
+     const { data } = await verifyOtp({ otp, phone: '', hash: '' }) 
+    } catch (error) {
+      console.log(error); 
+    }
     // onNext()
   }
 
