76fcd5aa87f4bc0d4fabe5c885e54bb62a0ec93c
joaquin <joaquinre123@gmail.com>
2022-09-05 20:45:04 -0300

    StepAvatar css

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../../components/shared/Button/Button'
+import Card from '../../../components/shared/Card/Card'
+import styles from "./StepAvatar.module.css";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const StepAvatar = ({ onNext }) => {
+  const {name} = useSelector((state) => state.activate)
+  const [image, setImage] = useState('/images/monkey-avatar.png')
+  function submit(params) {
+    
+  }
   return (
     <>
-      <div>StepAvatar</div>
-      <button onClick={onNext}>Next</button>
+      <Card title={`Okay, ${name}!`} icon='monkey-emoji'>
+          <p className={styles.subHeading}>How's this photo?</p>
+            <div className={styles.avatarWrapper}>
+              <img
+                className={styles.avatarImage}
+                src={image} 
+                alt="avatar"
+              />
+            </div>
+            <div>
+              <input
+                id='avatarInput' 
+                type="file" 
+                className={styles.avatarInput}
+              />
+              <label className={styles.avatarLabel} htmlFor="avatarInput">
+                Choose a diferent photo
+              </label>
+            </div>
+            <div>
+              <Button onClick={submit} text='Next'/>
+            </div>
+      </Card>
     </>
   )
 }
