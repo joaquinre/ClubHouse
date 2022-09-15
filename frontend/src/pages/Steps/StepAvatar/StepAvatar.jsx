63dd0916e731906f91159a8eeb33f16e0e023fcb
joaquin <joaquinre123@gmail.com>
2022-09-15 16:34:55 -0300

    fields validation

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -25,6 +25,7 @@ const StepAvatar = ({ onNext }) => {
     }
   }
   async function submit() {
+    if (!name || !avatar) return
     setLoading(true)
     try {
       const { data } = await activate({ name, avatar }) 
