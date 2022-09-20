409c4f38250ebcf9ea0055d3f4698278fa878c8d
joaquin <joaquinre123@gmail.com>
2022-09-20 11:05:44 -0300

    room card style bug fix

--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import styles from './RoomCard.module.css'
 
 export const RoomCard = ({ room }) => {
+    const history = useHistory()
   return (
-    <div className={styles.card}>
+    <div 
+        onClick={() => {
+            history.push(`/room/${room.id}`)
+        }} 
+        className={styles.card}
+    >
         <h3 className={styles.topic}>{room.topic}</h3>
-        <div className={styles.speakers}>
+        <div 
+            className={`${styles.speakers} ${
+                room.speakers.length === 1 ? styles.singleSpeaker : ''
+            }`}
+        >
             <div className={styles.avatars}>
                 {room.speakers.map(speaker => (
                     <img key={speaker.id} src={speaker.avatar} alt="speaker-avatar" />
--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -37,20 +37,23 @@ const Navigation = () => {
             <span style={logoText}>CodersHouse</span>
         </Link>
         {isAuth && (
-        <div className={styles.navRight}>
-            <h3>{user.name}</h3>
-            <Link to='/'>
-                <img 
-                    className={styles.avatar}
-                    src={user.avatar}
-                    width='40' height='40' 
-                    alt='avatar'
-                    />
-            </Link>
-            <button className={styles.logoutButton} onClick={logoutUser}>
-                <img src="/images/logout.png" alt="logout" />
-            </button>
-        </div>
+            <div className={styles.navRight}>
+                <h3>{user?.name}</h3>
+                {user.avatar && (
+                    <Link to='/'>
+                        <img 
+                            className={styles.avatar}
+                            src={user.avatar}
+                            width='40'
+                            height='40' 
+                            alt='avatar'
+                            />
+                    </Link>
+                )}
+                <button className={styles.logoutButton} onClick={logoutUser}>
+                    <img src="/images/logout.png" alt="logout" />
+                </button>
+            </div>
         )}
     </nav>
   )
