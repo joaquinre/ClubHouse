c6a141a139bcbcd28a08d4de15e98327370c5cf6
joaquin <joaquinre123@gmail.com>
2022-09-20 11:10:39 -0300

    Navigation fixs on activate page

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -39,17 +39,15 @@ const Navigation = () => {
         {isAuth && (
             <div className={styles.navRight}>
                 <h3>{user?.name}</h3>
-                {user.avatar && (
                     <Link to='/'>
                         <img 
                             className={styles.avatar}
-                            src={user.avatar}
+                            src={user.avatar ? user.avatar : '/images/monkey-avatar.png'}
                             width='40'
                             height='40' 
                             alt='avatar'
                             />
                     </Link>
-                )}
                 <button className={styles.logoutButton} onClick={logoutUser}>
                     <img src="/images/logout.png" alt="logout" />
                 </button>
